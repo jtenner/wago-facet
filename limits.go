48acda9322e332be193f6f14cd9f8d1ba365912a
Joshua Tenner <tenner.joshua@gmail.com>
2026-08-23 00:35:57 -0400

    style: format resource limits

--- limits.go
+++ limits.go
@@ -4,8 +4,8 @@ package facet
 // counts or lengths. These are implementation limits, not Facet ABI constants.
 // Exceeding one returns ERR_QUOTA before the corresponding host allocation.
 const (
-	maxIOVecs             uint32 = 1024
-	maxVectorBytes        uint64 = 16 << 20
-	maxTextUnits          uint64 = 1 << 20
-	maxPollSubscriptions         = 4096
+	maxIOVecs            uint32 = 1024
+	maxVectorBytes       uint64 = 16 << 20
+	maxTextUnits         uint64 = 1 << 20
+	maxPollSubscriptions        = 4096
 )
